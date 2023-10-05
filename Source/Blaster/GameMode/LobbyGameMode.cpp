1c48632f5250f52d3f628d3f43eea7b2083aee67
Alexander Medinets <fruitnex@gmail.com>
2023-10-05 18:45:52 +0300

    3.11 Network Role

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -8,7 +8,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
-	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+	int NumberOfPlayers = GameState.Get()->PlayerArray.Num();
 	if (NumberOfPlayers == 2)
 	{
 		UWorld* World = GetWorld();
